b8eafd05e7522649846c5e7579695c64eee852ab
Matheus Henrique de Sousa Cruz <matheus.cruz@radixeng.com.br>
2020-11-19 23:55:51 -0300

    first commit

---
+++ hello/src/hello.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	var name string = "Matheus"
+	var version float32 = 1.1
+
+	var nameInf = "Cruz"
+	var versionInf = 1.3
+
+	fmt.Println("Hello, Sir", name)
+	fmt.Println("Application's version: ", version)
+
+	fmt.Println("Variable type is: ", reflect.TypeOf(name))
+	fmt.Println("Variable type is: ", reflect.TypeOf(version))
+	fmt.Println("Variable type is: ", reflect.TypeOf(nameInf))
+	fmt.Println("Variable type is: ", reflect.TypeOf(versionInf))
+}
